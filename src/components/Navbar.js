5b97726ce08271bd549ab8350d0a23027255e8b4
Srikar Kumar <145329728+SrikarKumar20@users.noreply.github.com>
2026-03-28 16:29:05 +0530

    Create Navbar.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav style={{ padding: "10px", background: "#eee" }}>
+      <span style={{ marginRight: "10px" }}>Home</span>
+      <span style={{ marginRight: "10px" }}>About</span>
+      <span>Contact</span>
+    </nav>
+  );
+}
+
+export default Navbar;
