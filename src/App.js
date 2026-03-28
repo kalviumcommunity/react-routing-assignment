72529bcea82e8f595cac58af467ee0b13250a5e4
Srikar Kumar <145329728+SrikarKumar20@users.noreply.github.com>
2026-03-28 16:38:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from "react";
+
+// TODO 1: Import BrowserRouter, Routes, Route from react-router-dom
+
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
+
+// TODO 2: Import all pages (Home, About, Contact)
 
 function App() {
   return (
     <div>
+      {/* TODO 3: Wrap everything inside BrowserRouter */}
+
       <Navbar />
-      <Home />
+
+      {/* TODO 4: Replace this with Routes */}
+      {/* 
+        Create routes:
+        "/" → Home
+        "/about" → About
+        "/contact" → Contact
+      */}
+
     </div>
   );
 }
